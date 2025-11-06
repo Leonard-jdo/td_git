3784022e27a614281b9639d1e75165b51024ea8e
Leonard Joubert des Ouches <leonard.joubert_des_ouches@etu.minesparis.psl.eu>
2025-11-06 11:21:13 +0100

    le conflit a été réglé, on avance le main

--- test-conflit.py
+++ test-conflit.py
@@ -1,5 +1,5 @@
 #fichier python qui va servir de test de conflit
-j'ajoute des trucs dans le main pour faire un clonfli
+#j'ajoute des trucs dans le main pour faire un clonflit
 def f2(n):
 #J'ajoute des trucs pour faire un conflit
 def f(n):
